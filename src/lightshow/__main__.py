b6717bc5059f19539f48c899c135af485ca75e6e
Michael Green <self@michaelgreen.dev>
2021-12-08 18:41:19 -0800

    200 pixels

--- src/lightshow/__main__.py
+++ src/lightshow/__main__.py
@@ -7,19 +7,19 @@ from .window.circle import circle as window_circle
 from .window.comets import comets as window_comets
 from .tree.comets import comets as tree_comets
 
-PIXEL_COUNT = 50
-
 def main():
-    pixels = neopixel.NeoPixel(board.D18, PIXEL_COUNT, brightness=1)
-    pixels.fill((0, 0, 0))
     if sys.argv[-1] == "off":
         return
     _, target, effect = sys.argv
     if target == "window":
+        pixels = neopixel.NeoPixel(board.D18, 50, brightness=1)
+        pixels.fill((0, 0, 0))
         if effect == "comets":
             window_comets(pixels)
         elif effect == "circle":
             window_circle(pixels)
     elif target == "tree":
+        pixels = neopixel.NeoPixel(board.D18, 200, brightness=1)
+        pixels.fill((0, 0, 0))
         if effect == "comets":
             tree_comets(pixels)
