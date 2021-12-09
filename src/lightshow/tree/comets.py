141274ecda549a5672f923da3465764ac577851f
Michael Green <self@michaelgreen.dev>
2021-12-09 01:42:53 -0800

    decrease tree delay

--- src/lightshow/tree/comets.py
+++ src/lightshow/tree/comets.py
@@ -9,7 +9,7 @@ def comets(pixels):
         Comet(50, -1, 40, (0, 255, 0), pixels, background=(100, 100, 100)),
         Comet(100, -1, 40, (255, 0, 0), pixels, background=(100, 100, 100)),
         Comet(150, -1, 40, (0, 255, 0), pixels, background=(100, 100, 100)),
-        Terminal(pixels, delay=0.1),
+        Terminal(pixels, delay=0.25),
     ]
     for comet in itertools.cycle(comets):
         next(comet)
