5e4d5937a8ddebcfc038a6b8e7a190cfec29a210
Michael Green <self@michaelgreen.dev>
2021-12-08 18:50:40 -0800

    update tree

--- src/lightshow/tree/comets.py
+++ src/lightshow/tree/comets.py
@@ -4,10 +4,10 @@ from ..objects import Comet
 
 def comets(pixels):
     comets = [
-        Comet(0, -1, 10, (255, 0, 0), pixels),
-        Comet(50, -1, 10, (0, 255, 0), pixels),
-        Comet(100, -1, 10, (255, 0, 0), pixels),
-        Comet(150, -1, 10, (0, 255, 0), pixels),
+        Comet(0, -1, 40, (255, 0, 0), pixels, background=(100,100,100)),
+        Comet(50, -1, 40, (0, 255, 0), pixels, background=(100,100,100)),
+        Comet(100, -1, 40, (255, 0, 0), pixels, background=(100,100,100)),
+        Comet(150, -1, 40, (0, 255, 0), pixels, background=(100,100,100)),
     ]
     for comet in itertools.cycle(comets):
         next(comet)
