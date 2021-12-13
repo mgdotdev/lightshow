309195e9142b577e17c4d91cd5539ac25a4e5b80
Michael Green <self@michaelgreen.dev>
2021-12-13 06:52:36 -0800

    more lights, bg

--- src/lightshow/tree/comets.py
+++ src/lightshow/tree/comets.py
@@ -2,7 +2,7 @@ import itertools
 
 from ..objects import Comet, Terminal
 
-BACKGROUND = (0, 0, 0)
+BACKGROUND = (10, 10, 10)
 
 GREEN = (255, 0, 0)
 RED = (0, 255, 0)
@@ -21,6 +21,8 @@ def comets(pixels):
         Comet(175, 1, TAIL_LENGTH, RED, pixels, background=BACKGROUND),
         Comet(200, 1, TAIL_LENGTH, RED, pixels, background=BACKGROUND),
         Comet(225, 1, TAIL_LENGTH, RED, pixels, background=BACKGROUND),
+        Comet(250, 1, TAIL_LENGTH, GREEN, pixels, background=BACKGROUND),
+        Comet(275, 1, TAIL_LENGTH, RED, pixels, background=BACKGROUND),
         Terminal(pixels, delay=0.05),
     ]
 
