39d035f03bde4f49ce03f91853875f5afd326f36
Michael Green <self@michaelgreen.dev>
2021-12-14 09:56:00 -0800

    add pc controller

--- src/lightshow/__main__.py
+++ src/lightshow/__main__.py
@@ -3,10 +3,10 @@ import sys
 import board
 import neopixel
 
+from .pc.circle import circle as pc_circle
+from .tree.comets import comets as tree_comets
 from .window.circle import circle as window_circle
 from .window.comets import comets as window_comets
-from .tree.comets import comets as tree_comets
-
 
 def main():
     if sys.argv[-1] == "off":
@@ -26,3 +26,8 @@ def main():
         pixels.fill((0, 0, 0))
         if effect == "comets":
             tree_comets(pixels)
+    elif target == "pc":
+        px1 = neopixel.NeoPixel(board.D18, 13, brightness=1, auto_write=False)
+        px2 = neopixel.NeoPixel(board.D21, 13, brightness=1, auto_write=False)
+        if effect == "circle":
+            pc_circle(px1, px2)
---
+++ src/lightshow/pc/circle.py
@@ -0,0 +1,22 @@
+import time
+import itertools
+
+PIXELS = 13
+
+def circle_indexes(center, spread, pixel_count, offset=0):
+    return tuple(
+        (i % pixel_count)
+        for i in range(center - spread + offset, center + spread + offset + 1)
+    )
+
+
+def circle(px1, px2):
+    for i in itertools.cycle(range(PIXELS)):
+        span = 12
+        _cyan = circle_indexes(i, span, PIXELS)
+        for pixels in (px1, px2):
+            for count, index in enumerate(_cyan, start=-1 * span):
+                pixels[index] = (0, 255 - abs(int(count / span * 255)), 0)
+
+        time.sleep(0.05)
+        pixels.show()
