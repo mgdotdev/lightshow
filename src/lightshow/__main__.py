e7b42d324adacb4708703263d6d24bd113acfe96
Michael Green <self@michaelgreen.dev>
2021-12-14 12:08:03 -0800

    autofind hostname

--- src/lightshow/__main__.py
+++ src/lightshow/__main__.py
@@ -1,3 +1,4 @@
+import socket
 import sys
 
 import board
@@ -8,26 +9,33 @@ from .tree.comets import comets as tree_comets
 from .window.circle import circle as window_circle
 from .window.comets import comets as window_comets
 
+OFF = "off"
+
 def main():
-    if sys.argv[-1] == "off":
-        pixels = neopixel.NeoPixel(board.D18, 1000)
-        pixels.fill((0, 0, 0))
-        return
-    _, target, effect = sys.argv
-    if target == "window":
+    target = socket.gethostname()
+    _, effect = sys.argv
+    if target == "apple":
         pixels = neopixel.NeoPixel(board.D18, 50, brightness=1, auto_write=False)
         pixels.fill((0, 0, 0))
-        if effect == "comets":
+        if effect == OFF:
+            return        
+        elif effect == "comets":
             window_comets(pixels)
         elif effect == "circle":
             window_circle(pixels)
-    elif target == "tree":
+    elif target == "mud":
         pixels = neopixel.NeoPixel(board.D18, 300, brightness=1, auto_write=False)
         pixels.fill((0, 0, 0))
-        if effect == "comets":
+        if effect == OFF:
+            return
+        elif effect == "comets":
             tree_comets(pixels)
-    elif target == "pc":
+    elif target == "keylime":
         px1 = neopixel.NeoPixel(board.D18, 13, brightness=1, auto_write=False)
         px2 = neopixel.NeoPixel(board.D21, 13, brightness=1, auto_write=False)
-        if effect == "circle":
+        px1.fill((0,0,0))
+        px2.fill((0,0,0))
+        if effect == OFF:
+            return
+        elif effect == "circle":
             pc_circle(px1, px2)
