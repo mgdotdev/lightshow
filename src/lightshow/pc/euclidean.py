38ba0e98c4d24c7d3a83c544ba443aa17caf83d9
Michael Green <self@michaelgreen.dev>
2021-12-18 12:24:20 -0800

    tweaks

--- src/lightshow/pc/euclidean.py
+++ src/lightshow/pc/euclidean.py
@@ -9,6 +9,7 @@ COLORS = [
     (255,60,0),
     (255,80,0),
     (255,120,0),
+    (255,160,0),
     (255,255,0),
 ]
 
@@ -41,7 +42,7 @@ def fire(bottom, top):
         bottom.fill((255,0,0))
         top.fill((255,50,0))
         for spark in sparks:
-            spark.step(dx=0, dy=0.0125)
+            spark.step(dx=0, dy=0.02)
         for point in points:
             point.update(sparks)
         bottom.show()
