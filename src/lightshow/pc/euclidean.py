b90e54c6bf62bf278fa65cd8c334d6f9365ac7a8
Michael Green <self@michaelgreen.dev>
2021-12-18 11:16:34 -0800

    two sparks

--- src/lightshow/pc/euclidean.py
+++ src/lightshow/pc/euclidean.py
@@ -19,7 +19,7 @@ def fire(bottom, top):
 
     points = list(itertools.chain(bottom_points, top_points))
 
-    sparks = [Spark((255,255,255), 0.5, 0.0)]
+    sparks = [Spark((255, 0, 0), 0.75, 0.0), Spark((0,0,255), 0.25, 0.0)]
 
     while True:
         bottom.clear()
@@ -42,7 +42,7 @@ def pos_from_center(position, index, radius):
 
 def color_from_distance(color, distance):
     """using an exponential decay function to calculate falloff"""
-    return tuple(int(c * math.e**(-5*distance)) for c in color)
+    return tuple(int(c * math.e**(-10*distance)) for c in color)
 
 
 def euclidean_distance(point, spark):
@@ -75,4 +75,3 @@ class Spark(Coordinate):
     def step(self, dx, dy):
         self.x += dx
         self.y += dy
-        print(f"COORDINATES: {(self.x, self.y)}")
