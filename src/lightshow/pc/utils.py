edfebf8c6ae6d370d605e19ccac19c38224b8598
Michael Green <self@michaelgreen.dev>
2021-12-18 12:36:40 -0800

    tweaks

--- src/lightshow/pc/utils.py
+++ src/lightshow/pc/utils.py
@@ -2,8 +2,13 @@ CIRCUMFERENCE = 13
 
 
 def color_add(target, item):
+    raw = (sum(a,b) for (a, b) in zip(target, item))
     return tuple(
-        int(i) if (i := sum((a, b))) < 255 else 255 for (a, b) in zip(target, item)
+        int(i) 
+        if 0 < i <= 255 
+        else 0 if i < 0
+        else 255
+        for i in raw  
     )
 
 
